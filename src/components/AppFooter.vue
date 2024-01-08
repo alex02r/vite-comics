3ccbb6e3f4b8d5daa96665e5ea34d0a8b14449c7
alex02r <alex.giochi02@gmail.com>
2024-01-08 15:38:18 +0100

    [UPGRADE]Add section for AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,9 +4,17 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
+    <div class="blu-list">
         
     </div>
+    <footer>
+        <div class="top-footer">
+
+        </div>
+        <div class="bottom-footer">
+
+        </div>
+    </footer>
 </template>
 <style lang="scss" scoped>
     
