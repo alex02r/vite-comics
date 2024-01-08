8bb361288006f94b8282f3fb41b70e1ea3d0d99f
alex02r <alex.giochi02@gmail.com>
2024-01-08 15:31:35 +0100

    [UPGRADE]Add style to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,13 +6,24 @@ export default {
 <template lang="">
     <header>
         <div class="logo">
-
+            <img src="../assets/img/dc-logo.png" alt="logo">
         </div>
         <div class="nav-list">
-            
+
         </div>
     </header>
 </template>
-<style lang="">
-    
+<style lang="scss">
+    header{
+        display: flex;
+        justify-content: space-between;
+        .logo img{
+            width: 50px;
+        }
+        .nav-list{
+            width: 900px;
+            height: 50px;
+            background-color: aqua;
+        }
+    }
 </style>
