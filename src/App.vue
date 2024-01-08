c50b138d9fc0ac260b69cc293e8db713e5cbeeeb
alex02r <alex.giochi02@gmail.com>
2024-01-08 15:37:55 +0100

    [UPGRADE] Link AppFooter.vue to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
 export default {
   components:{
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 }
 </script>
 <template lang="">
   <div class="container">
     <AppHeader></AppHeader>
+    <AppFooter></AppFooter>
   </div>
 </template>
 <style lang="scss" scoped>
